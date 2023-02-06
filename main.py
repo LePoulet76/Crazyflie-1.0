a283934325f819b5599f098b1bddd49bb327f1ec
LePoulet76 <poulain.arthur@orange.fr>
2023-02-06 13:13:05 +0100

    Added connection to the Crazyflie in main

--- main.py
+++ main.py
@@ -14,3 +14,11 @@ available = cflib.crtp.scan_interfaces()
 print ("Crazyflies found:")
 for i in available:
     print(i[0])
+
+#Connexion au drone Crazyflie
+if len(available) > 0:
+    le = cflib.crazyflie.Crazyflie(rw_cache='./cache')
+    le.open_link(available[0][0])
+    print("Crazyflie connected")
+else:
+    print("No Crazyflies found, cannot run example")
